e03d99838c54265e1ff43603eb9e30c257fb3fc7
ozgur <ozgur@DESKTOP-BOAMFN0>
2022-02-09 03:08:00 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary <Tkey,Tvalue>
+    {
+        Tkey[] keys,tempKeys;
+        Tvalue[] values,tempValues,emptyArray;
+        
+        public MyDictionary()
+        {
+            emptyArray = new Tvalue[1];
+            keys = new Tkey[0];
+            values = new Tvalue[0];
+        }
+        public void Add(Tkey key, Tvalue value)
+        {
+            bool noException = true;
+
+            // same key adding control
+            for (int i = 0; i < keys.Length; i++)
+            {
+                if (keys[i].Equals(key))
+                {
+                    Console.WriteLine("001 There is same key in the dictionary!\nPlease try to add another key.");
+                    noException = false;
+                }
+            }
+
+            // key, value pair both were added
+            if (noException)
+            {
+                tempKeys = keys;
+                tempValues = values;
+                keys = new Tkey[keys.Length + 1];
+                values = new Tvalue[values.Length + 1];
+                for (int idx = 0; idx < tempKeys.Length || idx < tempValues.Length; idx++)
+                {
+                    keys[idx] = tempKeys[idx];
+                    values[idx] = tempValues[idx];
+                }
+                keys[keys.Length - 1] = key;
+                values[values.Length - 1] = value;
+            }  
+        }
+        // There is overloading on GetKeys method.
+
+        // return keys to Tkey array argument option
+        public void GetKeys(out Tkey[] keyArray)
+        {
+           keyArray = new Tkey[keys.Length];
+           for (int i = 0; i < keyArray.Length; i++)
+            {
+             keyArray[i] = keys[i];
+            } 
+        }
+        // given 0 argument to function option
+        public void GetKeys()
+        {
+            foreach (Tkey item in keys)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public Tvalue GetValueByKey(Tkey key)
+        {  
+            for (int idx = 0; idx < keys.Length; idx++)
+            {
+                if (keys[idx].Equals(key))
+                {
+                    return values[idx];
+                }
+                else if(idx == keys.Length-1)
+                {
+                    Console.WriteLine("002 key cannot found in the dictionary!");
+                }
+            }
+            return emptyArray[0];
+
+        }  
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string, string> cities = new MyDictionary<string, string>();
+            cities.Add("Ankara", "Çankaya");
+            cities.Add("Kırıkkale", "Yenimahalle");
+            cities.Add("İstanbul", "Ataşehir");
+
+            // Control of adding same key to dictionary
+            cities.Add("İstanbul", "Ataşehir");
+
+            Console.WriteLine("------------------------------------");
+            // Using GetKeys with value return function
+            string[] result;
+            cities.GetKeys(out result); // Returned keys to result argument.
+
+            // Using GetKeys with list to keys function
+            cities.GetKeys();
+
+            Console.WriteLine("------------------------------------");
+            // Using GetValueByKey method with exist and non-exist keys.
+            Console.WriteLine(cities.GetValueByKey("Ankara")); // for exist key
+            Console.WriteLine(cities.GetValueByKey("Antalya")); // for non-exist key
+        }
+    } 
+}
